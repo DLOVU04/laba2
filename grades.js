6fc81aceec10cef4b01d91ca6e01a4f7d78504fd
stanislavpetin <stanislavpetin16@gmail.com>
2026-06-10 13:45:17 +0300

    feat: данные

---
+++ grades.js
@@ -0,0 +1,9 @@
+const grades = [
+    { name: "Макар", score: 85 },
+    { name: "Денис", score: 92 },
+    { name: "Анна", score: 78 },
+    { name: "Даша", score: 88 },
+    { name: "Студент_X", score: 45 }
+];
+
+export default grades;
